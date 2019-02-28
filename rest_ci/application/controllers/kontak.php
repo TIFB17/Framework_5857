2ed4524979138e094979e029d0addd8703f94220
abdilanamohtalia <44762945+abdilanamohtalia@users.noreply.github.com>
2019-02-28 15:57:42 +0700

    posthafidzabdi
    hafidz abdi jos gandos kotos kotos

--- rest_ci/application/controllers/kontak.php
+++ rest_ci/application/controllers/kontak.php
@@ -58,6 +58,20 @@ class Kontak extends REST_Controller {
 
     //Masukan function selanjutnya disini
     
+//Mengirim atau menambah data kontak baru
+    function index_post() {
+        $data = array(
+                    'id'           => $this->post('id'),
+                    'nama'          => $this->post('nama'),
+                    'nomor'    => $this->post('nomor'));
+        $insert = $this->db->insert('telepon', $data);
+        if ($insert) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
 
+    //Masukan function selanjutnya disini
 }
 ?>
