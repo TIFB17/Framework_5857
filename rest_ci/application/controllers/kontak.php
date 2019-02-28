ee6de099cdad48969c9b3e3bf462a6e00ae3350f
@novia318 <novvianurul@gmail.com>
2019-02-28 15:43:45 +0700

    novia (put)

--- rest_ci/application/controllers/kontak.php
+++ rest_ci/application/controllers/kontak.php
@@ -37,5 +37,27 @@ class Kontak extends REST_Controller {
             $this->response(array('status' => 'fail', 502));
         }
     }
+    //koding selanjutnya taruh dibawah
+    //novia (put)
+
+    //Memperbarui data kontak yang telah ada
+    function index_put() {
+        $id = $this->put('id');
+        $data = array(
+                    'id'       => $this->put('id'),
+                    'nama'          => $this->put('nama'),
+                    'nomor'    => $this->put('nomor'));
+        $this->db->where('id', $id);
+        $update = $this->db->update('telepon', $data);
+        if ($update) {
+            $this->response($data, 200);
+        } else {
+            $this->response(array('status' => 'fail', 502));
+        }
+    }
+
+    //Masukan function selanjutnya disini
+    
+
 }
 ?>
